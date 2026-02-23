c1d98372e82848c713f6a6f2d27189ef330678fb
dahiana <quinterodahiana42@gmail.com>
2026-02-23 13:58:46 -0500

    Refactor authentication decorators in movimientos routes for consistency

--- app/movimientos/routes.py
+++ app/movimientos/routes.py
@@ -27,20 +27,20 @@ from app.movimientos.services import (
 from  app.utils.security import login_required
 
 @movimientos_bp.route('/')
-    @login_required()
+@login_required
 def index():
     movimiento = listar_movimientos()
     return render_template('movimientos/index.html',movimiento = movimiento)
 
 
 @movimientos_bp.route('/create')
-    @login_required()
+@login_required
 def create():
     return render_template('movimientos/create.html')
 
 
 @movimientos_bp.route('movomientos /guardar', methods=['GET', 'POST'])
-    @login_required()
+@login_required
 def guardar():
     if request.method == 'POST':
         data = {
@@ -60,7 +60,7 @@ def guardar():
 
 
 @movimientos_bp.route('movimientos/buscar_por_cedula/<cedula>')
-    @login_required()
+@login_required
 def buscar_por_cedula(cedula):
     
     cliente = service_buscar_por_cedula(cedula)
@@ -76,12 +76,12 @@ def buscar_por_cedula(cedula):
     })
 
 @movimientos_bp.route('/edit')
-    @login_required()
+@login_required
 def edit():
     return render_template('movimientos/edit.html')
 
 @movimientos_bp.route('/delete')
-    @login_required()
+@login_required
 def delete():
     return render_template('movimientos/delete.html')
 
